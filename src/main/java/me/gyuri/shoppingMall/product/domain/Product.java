be24cf028c2263300ef5ca54851d240e41854c91
devgyuri <gksmf910@gmail.com>
2024-07-02 16:08:15 +0900

    connect MySQL

---
+++ src/main/java/me/gyuri/shoppingMall/product/domain/Product.java
@@ -0,0 +1,35 @@
+package me.gyuri.shoppingMall.product.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "quantity", nullable = false)
+    @ColumnDefault("0")
+    private Integer quantity;
+
+    @Builder
+    public Product(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, price, quantity) VALUES ("상품1", 3000, 10);
+INSERT INTO product (name, price, quantity) VALUES ("상품2", 5000, 20);
+INSERT INTO product (name, price, quantity) VALUES ("상품3", 1000, 30);
